659337df34d2309eba75c23af0c54d4805711d56
VuTuanNghia <nghiatuanvu2006@gmail.com>
2025-02-20 09:09:41 +0700

    updates some new commands

--- MortgageCalculator/MortgageCalculator.java
+++ MortgageCalculator/MortgageCalculator.java
@@ -1,21 +1,41 @@
-import java.awt.*;
 import java.text.NumberFormat;
-import java.util.Arrays;
-import java.util.Date;
 import java.util.Scanner;
 
-public class Hello{
+public class MortgageCalculator{
     public static void main(String[] args) {
-        System.out.print("Principal: ");
         Scanner scan = new Scanner(System.in);
-        int prin = scan.nextInt();
-        System.out.print("Annual Interest Rate: ");
-        double air = scan.nextDouble();
-        System.out.print("Period (Years): ");
-        byte years = scan.nextByte();
+        int prin;
+        float air;
+        byte years;
+        do {
+            System.out.print("Principal ($1K - $1M): ");
+            prin = scan.nextInt();
+            if (prin >= 1000 && prin <= 1000000){
+                break;
+            }
+            System.out.println("Enter a number between 1,000 and 1,000,000.");
+        }
+        while (prin < 1000 || prin > 1000000);
+        do {
+            System.out.print("Annual Interest Rate: ");
+            air = scan.nextFloat();
+            if (air > 0 && air <= 30){
+                break;
+            }
+            System.out.println("Enter a value greater than 0 and less than or equal to 30.");
+        }
+        while (air <= 0 || air > 30);
+        do {
+            System.out.print("Period (Years): ");
+            years = scan.nextByte();
+            if (years > 0 && years <= 30){
+                break;
+            }
+            System.out.println("Enter a value greater than 0 and less than or equal to 30.");
+        }
+        while (years <= 0 || years > 30);
         double mortNum = (prin * 0.01*air * Math.pow((1+0.01*air/12), years * 12)) / ((12* Math.pow((1+0.01 *  air / 12), years * 12)) - 12);
         String mortform = NumberFormat.getCurrencyInstance().format(mortNum);
         System.out.print("Morgage: " + mortform);
-
     }
 }
